8ce3d7ad21173693e944b1ed428ccce2f78f41d0
Cedric Thiebault <cedric.thiebault@sensefly.com>
2017-11-07 11:24:12 +0100

    Add debug to Docker entrypoint

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -3,4 +3,8 @@ set -e
 # https://unix.stackexchange.com/a/333870
 set -f
 
+echo "Configuration:" && \
+echo "  JAVA_OPTS:            $JAVA_OPTS" && \
+echo "  CLOUDWATCH_LOG_GROUP: $CLOUDWATCH_LOG_GROUP" && \
+echo "  RUN: java $JAVA_OPTS -jar /app.jar \"$@\"" && \
 java $JAVA_OPTS -jar /app.jar "$@"
